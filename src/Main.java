7dcc63e95f48a4339f27e55a0d02aef0738440e9
Waylon <weese.619547@swl.k12.oh.us>
2023-11-06 09:14:53 -0500

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Loop 1");
+        for (int i = 0; i <= 5; i++){
+            for(int j = i; j > 0; j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Loop 2");
+        for (int i = 5; i >= 0; i--){
+            for(int j = i; j > 0; j--){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("Loop 3");
+        for (int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 }
